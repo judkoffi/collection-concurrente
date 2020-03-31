a40af7bce7b6c566e028c0d354ddc8c0a2d10b62
jkoffi <judkoffi@gmail.com>
2020-03-31 10:58:17 +0200

    add lab3

---
+++ lab3/src/fr/umlv/conc/SpinLock.java
@@ -0,0 +1,64 @@
+package fr.umlv.conc;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+public class SpinLock {
+  // default value = false
+  private volatile boolean locked;
+
+  private static final VarHandle HANDLE;
+
+  static {
+    var lookup = MethodHandles.lookup();
+    try {
+      HANDLE = lookup.findVarHandle(SpinLock.class, "locked", boolean.class);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  public void lock() {
+    while (!HANDLE.compareAndSet(this, false, true)) {
+      Thread.onSpinWait();
+    }
+  }
+
+  public void unlock() {
+    // volatile write
+    locked = false;
+  }
+
+  public boolean tryLock() {
+    // on essaie de prend si on peu on le prends donc ol le passe a true
+    // sinon on l'a pas et on renvoie false
+    return HANDLE.compareAndSet(this, false, true);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    var runnable = new Runnable() {
+      private int counter;
+      private final SpinLock spinLock = new SpinLock();
+
+      @Override
+      public void run() {
+        for (int i = 0; i < 1_000_000; i++) {
+          spinLock.lock();
+          try {
+            counter++;
+          } finally {
+            spinLock.unlock();
+          }
+        }
+      }
+    };
+
+    var t1 = new Thread(runnable);
+    var t2 = new Thread(runnable);
+    t1.start();
+    t2.start();
+    t1.join();
+    t2.join();
+    System.out.println("counter " + runnable.counter);
+  }
+}
