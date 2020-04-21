1210ee5c930a2816d750078684dea58e286ca094
jkoffi <judkoffi@gmail.com>
2020-04-21 22:02:18 +0200

    add lab5 ci

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+
+if [ "$FOLDER" = "lab6" ]; then
+    if [ "$OSTYPE" = "darwin17" ]; then # OS = osx
+        wget https://github.com/forax/java-next/releases/download/untagged-bf24edb7ff6b12ce0d49/jdk-15-vector-osx.tar.gz
+        tar xvf jdk-15-vector-osx.tar.gz
+    else
+        wget https://github.com/forax/java-next/releases/download/untagged-bf24edb7ff6b12ce0d49/jdk-15-vector-linux.tar.gz
+        tar xvf jdk-15-vector-linux.tar.gz
+    fi
+    export JAVA_HOME=$(pwd)/jdk-15-vector/
+fi
+
+cd $FOLDER
+mvn package
